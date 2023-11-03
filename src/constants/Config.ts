74615675e5f18d217f8a149f7ffa7ccc38470d0e
ignaciosantise <ignacio.santise@gmail.com>
2023-11-03 11:26:27 -0300

    chore: updated sdk version constant

--- src/constants/Config.ts
+++ src/constants/Config.ts
@@ -8,7 +8,7 @@ const REQUIRED_EVENTS = ['chainChanged', 'accountsChanged'];
 export const CORE_VERSION = '2.10.4';
 
 // DO NOT REMOVE, SHOULD MATCH SDK PACKAGE VERSION
-export const SDK_VERSION = '1.0.0-rc.11';
+export const SDK_VERSION = '1.0.0-rc.12';
 
 export const defaultSessionParams: ISessionParams = {
   namespaces: {
