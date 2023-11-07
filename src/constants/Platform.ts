87e5e86157e6c1cfef909f6bf0ff8b34868a03a4
ignaciosantise <ignacio.santise@gmail.com>
2023-11-07 10:42:13 -0300

    chore: removed unnecesary char from regex

--- src/constants/Platform.ts
+++ src/constants/Platform.ts
@@ -4,4 +4,4 @@ export const isAndroid = Platform.OS === 'android';
 export const isIOS = Platform.OS === 'ios';
 
 export const PLAYSTORE_REGEX =
-  /^https?:\/\/(www\.)?play\.google\.com\/store\/apps\/details\?id=[\w\.]+(&[\w-]+(=[\w%-]*)?)*/;
+  /^https?:\/\/(www\.)?play\.google\.com\/store\/apps\/details\?id=[\w.]+(&[\w-]+(=[\w%-]*)?)*/;
