e42e0dc51b0e05d2ecd9a75f3e5839715312d26e
ignaciosantise <ignacio.santise@gmail.com>
2024-01-04 21:22:23 -0300

    refactor: removed unnecesary files

--- src/components/Image.tsx
+++ src/components/Image.tsx
@@ -3,8 +3,8 @@ import {
   Animated,
   Image as NativeImage,
   ImageProps as NativeProps,
+  Platform,
 } from 'react-native';
-import { isIOS } from '../constants/Platform';
 
 export type ImageProps = Omit<NativeProps, 'source'> & {
   source: string;
@@ -13,6 +13,7 @@ export type ImageProps = Omit<NativeProps, 'source'> & {
 
 function Image({ source, headers, style, ...rest }: ImageProps) {
   const opacity = useRef(new Animated.Value(0));
+  const isIOS = Platform.OS === 'ios';
 
   // Fade in image on load for iOS. Android does this by default.
   const onLoadEnd = () => {
